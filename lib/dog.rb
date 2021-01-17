b8d90252759c98c4788182732019139670a6df66
Carlos E. La Torre <amautacelt@gmail.com>
2021-01-17 00:29:07 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 class Dog
 
-    def name=(new_name)
-        @name = new_name
+    def name=(name)
+        @name = name
     end
 
     def name
         @name
     end
 
-    def breed=(new_breed)
-        @breed = new_breed
+    def breed=(breed)
+        @breed = breed
     end
 
     def breed
